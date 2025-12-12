d05e8a680d5bad008ce36b59891b6676b6abacbb
Migszydyzy <cedrickmiguelpenaflorida@gmail.com>
2025-12-12 16:54:17 +0800

    Update multiple files and apply code changes

--- models.py
+++ models.py
@@ -1,5 +1,4 @@
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -9,7 +8,7 @@ class WellnessEntry(db.Model):
     mood = db.Column(db.String(20), nullable=False)
     steps = db.Column(db.Integer)
     notes = db.Column(db.Text)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -17,24 +16,26 @@ class Event(db.Model):
     description = db.Column(db.Text)
     event_date = db.Column(db.String(20), nullable=False)
     location = db.Column(db.String(100), nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # created_at removed
+
 
 class ForumPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(50), nullable=False)
     title = db.Column(db.String(200), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # created_at removed
+
 
 class Comment(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    forum_post_id = db.Column(db.Integer, db.ForeignKey('forum_post.id'), nullable=False)  # Foreign key
+    forum_post_id = db.Column(db.Integer, db.ForeignKey('forum_post.id'), nullable=False)
     username = db.Column(db.String(50), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    # created_at removed
 
-    # Relationship to easily access the parent ForumPost
     forum_post = db.relationship('ForumPost', backref=db.backref('comments', lazy=True))
+
 
 
 
